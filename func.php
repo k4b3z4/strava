e4668e249978e8c050ce2ae179af7612c822e9b7
Federico Pedot <federico.pedot@xnlatam.com>
2023-05-11 16:02:06 -0300

    revert :(

--- func.php
+++ func.php
@@ -170,32 +170,6 @@ function traer_tracks(){
     logger("tracks: ". print_r($tracks,true));
 	
 	foreach($tracks as $track){
-
-        $stmt = mysqli_prepare($mysqli_link, "INSERT INTO tracks
-                                       (athlete,id,external_id,name,distance,
-                                        moving_time,total_elevation_gain,type,start_date_local,average_speed,
-                                        gear_id,location_city,location_state,location_country,achievement_count,
-                                        kudos_count,average_heartrate,max_heartrate,elev_high,elev_low,
-                                        start_latlng,end_latlng,workout_type,average_cadence,average_temp,
-                                        average_watts,suffer_score,calories,device_name,gear,
-                                        segment_efforts,highlighted_kudosers,track)
-                            VALUES (?,?,?,?,?,?,
-                                    ?,?,?,?,?,?,
-                                    ?,?,?,?,?,?,
-                                    ?,?,?,?,?,?,
-                                    ?,?,?,?,?,?,
-                                    ?,?,?)"
-                    );
-
-        mysqli_stmt_bind_param($stmt, 'ssssssssssssssiiiiddssidddidsbbbb',
-                            $athlete,$id,$external_id,$name,$distance,
-                            $moving_time,$total_elevation_gain,$type,$start_date_local,$average_speed,
-                            $gear_id,$location_city,$location_state,$location_country,$achievement_count,
-                            $kudos_count,$average_heartrate,$max_heartrate,$elev_high,$elev_low,
-                            $start_latlng,$end_latlng,$workout_type,$average_cadence,$average_temp,
-                            $average_watts,$suffer_score,$calories,$device_name,$gear,
-                            $segment_efforts,$highlighted_kudosers,$track_ 
-                    );
 		
 		$id = $track["id"];
 		$external_id = $track["external_id"];
@@ -237,11 +211,27 @@ function traer_tracks(){
 		$track_ = json_encode($track);
 
 
-        mysqli_stmt_execute($stmt);
+        $sql = "INSERT INTO tracks
+                        (athlete,id,external_id,name,distance,
+                        moving_time,total_elevation_gain,type,start_date_local,average_speed,
+                        gear_id,location_city,location_state,location_country,achievement_count,
+                        kudos_count,average_heartrate,max_heartrate,elev_high,elev_low,
+                        start_latlng,end_latlng,workout_type,average_cadence,average_temp,
+                        average_watts,suffer_score,calories,device_name,gear,
+                        segment_efforts,highlighted_kudosers,track)
+                VALUES ('$athlete','$id','$external_id','$name','$distance',
+                        '$moving_time','$total_elevation_gain','$type','$start_date_local','$average_speed',
+                        '$gear_id','$location_city','$location_state','$location_country',$achievement_count,
+                        $kudos_count,$average_heartrate,$max_heartrate,$elev_high,$elev_low,
+                        '$start_latlng','$end_latlng',$workout_type,$average_cadence,$average_temp,
+                        $average_watts,$suffer_score,$calories,'$device_name','$gear',
+                        '$segment_efforts','$highlighted_kudosers','$track_' )";
 
-        logger("Filas afectadas: " . mysqli_stmt_affected_rows($stmt));
+        $Resp= mysqli_query($mysqli_link,$sql);
 
-        mysqli_stmt_close($stmt);
+        if(!$Resp){
+            logger("Error sql: " . $sql);
+        }
 			
 	}
 	
